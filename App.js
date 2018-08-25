61a913841a0632ebb584e319cd63a7f98a5592b1
D0oBu <42699288+D0oBu@users.noreply.github.com>
2018-08-25 23:14:43 +0700

    Update App.js

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Eka Setiyanto</Text>
+        <Text>18</Text>
+        <Text>XI RPL 3</Text>
       </View>
     );
   }
